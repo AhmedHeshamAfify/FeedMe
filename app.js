7dc58e1a260514a1f8588472f10a5bd2e63955e6
Afify <a.heshamshaaban@gmail.com>
2020-02-01 00:15:50 -0600

    adding cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config()
 
 
@@ -15,6 +16,7 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 
 app.use('/users', usersRouter);
