55f9dc148bcbf045ad76bc10338d838330e3c4d3
Afify <a.heshamshaaban@gmail.com>
2020-02-01 00:40:19 -0600

    convert token object model

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,14 +3,14 @@ const usersService = require('../services/signUpService')
 const userSignUpController = (req, res, next) => {
     const user = req.body
     const token = usersService.signUp(user.userName, user.email, user.password);
-    res.status(200).send(token);
+    res.status(200).send({token : token});
 }
 const userSignInController = async (req, res, next) => {
 
     const user = req.body;
     const token = await usersService.signIn(user.email, user.password);
     if (token != false) {
-        res.status(200).send(token);
+        res.status(200).send({token : token});
     }
     else res.status(405).send("Access Not Allowed")
 }
