bc657b459b5c48dff46a10f693e3d6395235337e
Afify <a.heshamshaaban@gmail.com>
2020-01-31 23:12:15 -0600

    adding middlewares authentication

---
+++ middlewares/AuthMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1]
+    jwt.verify(token, process.env.SECRETE_KEY, (error, authData)=>{
+        if (error) {
+            error.message = 'Auth error'
+            next(error)
+        }
+        req.userData = authData
+        next()
+    })
+    
+}
+module.exports = { auth }
