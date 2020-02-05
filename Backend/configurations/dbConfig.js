68c9a5fb34fb41263246c12288baf3875327d839
msalah-mum <mshehata@mum.edu>
2020-02-05 00:23:18 -0600

    db connection string

--- Backend/configurations/dbConfig.js
+++ Backend/configurations/dbConfig.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
+//const dbConfig = require('./credential')
 
-mongoose.connect('mongodb+srv://root:123@cluster0-gxapi.mongodb.net/feedMe?retryWrites=true&w=majority')
+const pass = process.env.dbPassword
+const userName = process.env.dbUserName
+
+mongoose.connect("mongodb+srv://" + userName + ":" + pass + "@cluster0-gxapi.mongodb.net/feedMe?retryWrites=true&w=majority")
     .then(() => console.log('DB connected'))
     .catch(() => console.log('DB connection failed'))
 
